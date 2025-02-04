062525c40d654fda5d1721aa230f6d17a56efc3b
frostybee <rsleiman2@gmail.com>
2025-02-04 00:46:46 -0500

    Add creating multi-page website page

--- src/config/sidebars/main-sidebar.ts
+++ src/config/sidebars/main-sidebar.ts
@@ -54,6 +54,12 @@ export default
             directory: "css/Basics",
           }
         },
+        {
+          label: "Advanced CSS",
+          autogenerate: {
+            directory: "css/advanced",
+          }
+        },
       ],
     },
     {
